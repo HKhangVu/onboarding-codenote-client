ff32164212123a489b26fb2ff503518427286a75
HKhangVu <khang.vu@codelink.io>
2019-08-12 09:45:49 +0700

    fix indent

--- src/components/AuthenticatedRoute/index.tsx
+++ src/components/AuthenticatedRoute/index.tsx
@@ -3,19 +3,19 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from "react-router-dom";
 
 const AuthenticatedRoute = ({ component: C, isAuthenticated, ...rest }) =>
-	<Route
-		{...rest}
-		render={props =>
-			isAuthenticated
-				? <C {...props} />
-				: <Redirect
-					to={`/login?redirect=${props.location.pathname}${props.location
-						.search}`}
-				/>}
-	/>;
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated
+        ? <C {...props} />
+        : <Redirect
+          to={`/login?redirect=${props.location.pathname}${props.location
+            .search}`}
+        />}
+  />;
 
 const mapStateToProps = (state) => ({
-	isAuthenticated: state.authenticate.isAuthenticated,
+  isAuthenticated: state.authenticate.isAuthenticated,
 });
 
 export default connect(mapStateToProps)(AuthenticatedRoute);
